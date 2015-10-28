4582a686686d5f61568d6649b26c5de4cbd7e5a1
Sarah Kidd <sarah_k@live.com>
2015-10-28 14:44:23 -0400

    Create evenNumbers-py33.py

---
+++ 100/evenNumbers-py33.py
@@ -0,0 +1,15 @@
+'''
+	@author Sarah Kidd (www.linkedin.com/in/sarahkidd/)
+	Description of problem at: https://www.codeeval.com/open_challenges/100/
+'''
+
+import sys
+
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+	test = test.rstrip('\n')
+	num = int(test)
+	mod = num % 2
+	if mod == 1: print(0)
+	else: print(1)
+test_cases.close()
