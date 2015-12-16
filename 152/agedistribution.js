cf4ee1b0e9430d61735f43b4cdb008736233d497
Sarah Kidd <sarah_k@live.com>
2015-12-16 10:26:46 -0500

    Create agedistribution.js

---
+++ 152/agedistribution.js
@@ -0,0 +1,38 @@
+/*
+    @author Sarah Kidd (www.linkedin.com/in/sarahkidd/)
+    Description of problem at: https://www.codeeval.com/open_challenges/152/
+*/
+
+var fs = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line !== "") {
+        line = line.replace(/\r/gm, ""); // For Windows.
+        if (line >= 0 && line <= 2) {
+            console.log("Still in Mama's arms");
+        }
+        else if (line >= 3 && line <= 4) {
+            console.log("Preschool Maniac");
+        }
+        else if (line >= 5 && line <= 11) {
+            console.log("Elementary school");
+        }
+        else if (line >= 12 && line <= 14) {
+            console.log("Middle school");
+        }
+        else if (line >= 15 && line <= 18) {
+            console.log("High school");
+        }
+        else if (line >= 19 && line <= 22) {
+            console.log("College");
+        }
+        else if (line >= 23 && line <= 65) {
+            console.log("Working for the man");
+        }
+        else if (line >= 66 && line <= 100) {
+            console.log("The Golden Years");
+        }
+        else {
+            console.log("This program is for humans");
+        }
+    }
+});
